cdf57f408319f01ffab279050668034597d6e4f4
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2024-06-18 04:38:24 -0300

    CSS/JS: Eliminei os arquivos aviso.css e aviso.js relacionados ao aviso que alertava o usuário que o serviço estava em actualização.

--- fisiologia/aviso.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    const btnCloseAviso = document.querySelector(".btn-fechar-aviso");
-    btnCloseAviso.addEventListener("click", () => {
-        btnCloseAviso.parentElement.classList.remove("--open");
-    })
-})
